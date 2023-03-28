8ffcb4251ccb84aa88c2d09f9ad20e0cd0d2283f
Михаил Митрофанов <mmm606@inbox.ru>
2023-03-28 22:42:31 +0300

    Before new elipce alg

--- MCDApp/MainWindow.xaml.cs
+++ MCDApp/MainWindow.xaml.cs
@@ -43,33 +43,15 @@ namespace MCDApp
         void DrawShape(List<(int, int)> points)
         {
             ShemeChanvas.Children.Clear();
-            bool xMove = points.Any(i => i.Item1 < 0);
-            bool yMove = points.Any(x => x.Item2 < 0);
-            List<(int, int)> toDarw = new List<(int, int)>();
-            if (xMove || yMove)
-            {
-                int xMoveLen = 0; int yMoveLen = 0;
-                if (xMove)
-                {
-                    xMoveLen = -points.Select(i => i.Item1).Min();
-                }
-                if (yMove)
-                {
-                    yMoveLen = -points.Select(i => i.Item2).Min();
-                }
-                toDarw = points.Select(i => (i.Item1 + xMoveLen, i.Item2 + yMoveLen)).ToList();
-            }
-            else
-                toDarw = points;
-            ShemeChanvas.Width = toDarw.Select(i => i.Item1).Max() * blockSize;
-            ShemeChanvas.Height = toDarw.Select(i => i.Item2).Max() * blockSize;
-
-            foreach (var point in toDarw)
+            
+            ShemeChanvas.Width = points.Select(i => i.Item1).Max() * blockSize;
+            ShemeChanvas.Height = points.Select(i => i.Item2).Max() * blockSize;
+            foreach (var point in points)
             {
                 Border block = new Border();
                 block.Height = blockSize;
                 block.Width = blockSize;
-                block.BorderThickness = new Thickness(3);
+                block.BorderThickness = new Thickness(2);
                 block.Background = new SolidColorBrush(Colors.Red);
                 block.DataContext = point;
                 block.MouseEnter += (sender, e) => { CurrentBlock.Text = (sender as Border).DataContext.ToString(); };
@@ -86,10 +68,12 @@ namespace MCDApp
         {
             WStr.Text = ow.WidthStr;
             HStr.Text = ow.HeigthStr;
-            TopTB.Text = $"Сверху({ow.TopLimit})%";
-            BotTB.Text = $"Снизу({ow.BottomLimit})%";
-            LeftTB.Text = $"Слева({ow.LeftLimit})%";
-            RightTB.Text = $"Справа({ow.RightLimit})%";
+            TopTB.Text = $"Сверху(с {ow.TopLimit})";
+            BotTB.Text = $"Снизу(до {ow.BottomLimit})";
+            LeftTB.Text = $"Слева(с {ow.LeftLimit})";
+            RightTB.Text = $"Справа(до {ow.RightLimit})";
+
+            
 
         }
     }
--- MCDApp/OvalView.cs
+++ MCDApp/OvalView.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
 
 namespace MCDApp
 {
@@ -8,33 +13,49 @@ namespace MCDApp
     {
         int width = 10;
         int heigh = 10;
-        int top = 100;
-        int bottom = 100;
-        int left = 100;
-        int right = 100;
+        int top = 0;
+        int bottom = 10;
+        int left = 0;
+        int right = 10;
         public string WidthStr
         {
             get => width.ToString();
-            set => int.TryParse(value, out width);
+            set
+            {
+                int.TryParse(value, out width);
+                if (width < right)
+                    right = width;
+                Rebuild();
+            }
         }
         public string HeigthStr
         {
             get => heigh.ToString();
-            set => int.TryParse(value, out heigh);
+            set
+            {
+                int.TryParse(value, out heigh);
+                if (bottom > heigh)
+                    bottom = heigh;
+                Rebuild();
+            }
         }
         public int Width
         {
             get => width; set
             {
                 width = value;
+                if (width < right)
+                    right = width;
                 Rebuild();
             }
         }
-        public int Height
+        public int Heigth
         {
             get => heigh; set
             {
                 heigh = value;
+                if (bottom > heigh)
+                    bottom = heigh;
                 Rebuild();
             }
         }
@@ -74,17 +95,24 @@ namespace MCDApp
         void Rebuild()
         {
             var shape = new List<(int, int)>();
-            var len = 2 * Math.PI * Math.Sqrt((width * width + heigh * heigh) / 8);
+            var len = Math.PI * Math.Sqrt((width * width + heigh * heigh) / 8);
             var grade = (Math.Tau / len) / 5;
             for (double i = 0; i <= Math.Tau; i += grade)
             {
-                var sin = -(int)Math.Round((Math.Sin(i) * heigh));
-                var cos = (int)Math.Round((Math.Cos(i) * width));
+                var sin = (int)(Math.Round((Math.Sin(i) * heigh) / 2) + heigh / 2);
+                var cos = (int)(Math.Round((Math.Cos(i) * width) / 2) + width / 2);
                 shape.Add((cos, sin));
             }
+            shape = shape.Where(i => i.Item2 >= top && i.Item2 <= bottom && i.Item1 <= right && i.Item1 >= left).ToList();
             OnNewSheme.Invoke(shape);
-
         }
         public event ShemeUpdate OnNewSheme;
+
+        public double NormalizedLenth(double x, double y)
+        {
+            var hWidth = width / 2;
+            var hHeith = heigh / 2;
+            return new Vector2((float)(x / hWidth), (float)(y / hHeith)).Length();
+        }
     }
 }
